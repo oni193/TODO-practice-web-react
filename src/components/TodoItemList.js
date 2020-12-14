90aedbf21d4fdc1b3326b4592fc56d805bbb9528
oni193 <keykeykey193@gmail.com>
2020-12-14 17:00:29 +0900

    :bug: Fix a bug. Template 에 children을 주석처리 해서 안나왔었음.

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
-class TodoItemList extends Component{
-    render(){
-        const{ todos, onToggle, onRemove } = this.props;
+class TodoItemList extends Component {
+  render() {
+    const { todos, onToggle, onRemove } = this.props;
         // todos: todo 객체들이 들어가는 배열
         // onToggle: 체크박스를 키고 끄는 함수
         // onRemove: 아이템을 삭제시키는 함수
-        return (
-            <div>
-            <TodoItem text="안녕"/>
-            <TodoItem text="리액트"/>
-            <TodoItem text="방가"/>
-            </div>
-        );
-    }
-};
+    return (
+      <div>
+        <TodoItem text="안녕"/>
+        <TodoItem text="리액트"/>
+        <TodoItem text="반가워"/>        
+      </div>
+    );
+  }
+}
 
 export default TodoItemList;
