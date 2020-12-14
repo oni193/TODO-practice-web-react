8ead59fe831233bddc47c4dca3b4dc518e7230db
oni193 <keykeykey193@gmail.com>
2020-12-14 16:43:38 +0900

    💚 Refactor TodoItem ( css , js )

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
+import TodoItemList from './components/TodoItemList';
 import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
       <TodoListTemplate form = {<Form/>}>
-        템플릿 완성
+        <TodoItemList/>
       </TodoListTemplate>
     );
   }
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -3,10 +3,17 @@ import React, { Component } from 'react';
 class TodoItemList extends Component{
     render(){
         const{ todos, onToggle, onRemove } = this.props;
+        // todos: todo 객체들이 들어가는 배열
+        // onToggle: 체크박스를 키고 끄는 함수
+        // onRemove: 아이템을 삭제시키는 함수
         return (
             <div>
-                
+            <TodoItem text="안녕"/>
+            <TodoItem text="리액트"/>
+            <TodoItem text="방가"/>
             </div>
         );
     }
 };
+
+export default TodoItemList;
